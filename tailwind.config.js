804d9e09a953f46911cab207b8876b3dbac15067
124tranvita <124tranvita@gmail.com>
2022-10-15 02:09:56 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      width: {
+        1200: "1200px",
+      },
+      height: {
+        a3: "442px",
+        a4: "244px",
+        a5: "194px",
+        500: "500px",
+      },
+    },
+  },
+  plugins: [],
+};
